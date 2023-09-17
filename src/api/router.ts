88fa607d9e2565dde1ba29561f7e5e3d21bb620e
HukakHePak <valsidalvonilb@outlook.com>
2023-09-18 02:09:28 +0400

    feat: linux console, rem: download route, fix: download links

--- src/api/router.ts
+++ src/api/router.ts
@@ -1,11 +1,9 @@
 import express from "express"
 import carsRoute from "./routes/cars"
-import downloadRoute from "./routes/download"
 import errorHandler from "./middleware/errorHandler"
 import publicRoute from "./routes/public"
 const router = express.Router()
 
-router.use("/download", downloadRoute)
 router.use("/api", carsRoute)
 router.use("/", publicRoute)
 router.use(errorHandler)
